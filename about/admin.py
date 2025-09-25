9c0b8875b3fd009c7ac8e75e2ab955978530e72b
LORDx0001 <bexruztoj2005@gmail.com>
2025-09-25 17:14:34 +0500

    Zaybal settings2

--- about/admin.py
+++ about/admin.py
@@ -4,6 +4,6 @@ from .models import About
 
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
-    list_display = ("id", "title_en", "created_at")
-    search_fields = ("title_en", "title_uz", "title_ru")
+    list_display = ("id", "title_uz", "title_ru", "title_en", "created_at")
     list_filter = ("created_at",)
+    search_fields = ("title_uz", "title_ru", "title_en")
--- about/models.py
+++ about/models.py
@@ -2,15 +2,22 @@ from django.db import models
 
 
 class About(models.Model):
-    title_uz = models.CharField(max_length=255)
-    title_ru = models.CharField(max_length=255)
-    title_en = models.CharField(max_length=255)
+    # Title
+    title_uz = models.CharField(max_length=255, verbose_name="Title (Uzbek)")
+    title_ru = models.CharField(max_length=255, verbose_name="Title (Russian)")
+    title_en = models.CharField(max_length=255, verbose_name="Title (English)")
 
-    description_uz = models.TextField()
-    description_ru = models.TextField()
-    description_en = models.TextField()
+    # Description
+    description_uz = models.TextField(verbose_name="Description (Uzbek)")
+    description_ru = models.TextField(verbose_name="Description (Russian)")
+    description_en = models.TextField(verbose_name="Description (English)")
 
-    image = models.ImageField(upload_to="about/")
+    # Image
+    image = models.ImageField(upload_to="about/", verbose_name="Image")
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
 
     def __str__(self):
         return self.title_uz
