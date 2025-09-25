38cb12e1ba5af9cd63112900cf2f2ef6482caed1
LORDx0001 <bexruztoj2005@gmail.com>
2025-09-25 17:10:02 +0500

    Zaybal settings

--- about/apps.py
+++ about/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+class AboutConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "about"
