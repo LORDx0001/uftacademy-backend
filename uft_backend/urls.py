7b945c403a82cf319b6f563c7621a567c9103e54
LORDx0001 <bexruztoj2005@gmail.com>
2025-09-26 10:49:09 +0500

    Setting Urls

--- uft_backend/urls.py
+++ uft_backend/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('api/', include('contact.urls')),
     path('api/', include('footer.urls')),  # ⬅️ Footer app
     path('api/', include('header.urls')),
+    path('api/', include('title.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
