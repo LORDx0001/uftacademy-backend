e64a8f0ee6d8c512ba99495e169049a7695eb902
LORDx0001 <bexruztoj2005@gmail.com>
2025-09-26 11:46:49 +0500

    Titles fix

--- title/admin.py
+++ title/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Title
 
-
 @admin.register(Title)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ("id", "title_uz", "title_ru", "title_en")
-    search_fields = ("title_uz", "title_ru", "title_en")
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('key', 'title_uz', 'title_ru', 'title_en')
+    search_fields = ('key', 'title_uz', 'title_ru', 'title_en')
+    list_filter = ('key',)
--- title/models.py
+++ title/models.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
-from .models import Title
+from django.db import models
 
-@admin.register(Title)
-class TitleAdmin(admin.ModelAdmin):
-    list_display = ('key', 'title_uz', 'title_ru', 'title_en')
-    search_fields = ('key', 'title_uz', 'title_ru', 'title_en')
-    list_filter = ('key',)
+class Titles(models.Model):
+    
+    key = models.CharField(max_length=200, verbose_name="keys")
+    title_uz = models.CharField(max_length=255, verbose_name="Заголовок (UZ)")
+    title_ru = models.CharField(max_length=255, verbose_name="Заголовок (RU)")
+    title_en = models.CharField(max_length=255, verbose_name="Заголовок (EN)")
+    
+    def __str__(self):
+        return self.title_uz
